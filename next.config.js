30c84e247c1aacd9ae09d541f0854039c07d6822
Kraig Walker <kraig_walker@me.com>
2017-04-01 23:35:17 +0100

    analyze bundles in all envs

--- next.config.js
+++ next.config.js
@@ -1,19 +1,15 @@
-const prod = process.env.NODE_ENV === 'production';
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
+module.exports = {
+	webpack: (config, { dev }) => {
+	// Perform customizations to config
+	config.plugins.push(
+		new BundleAnalyzerPlugin({
+			analyzerMode: 'disabled',
+			// For all options see https://github.com/th0r/webpack-bundle-analyzer#as-plugin
+			generateStatsFile: true,
 
-if(!prod) {
-	const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
-	module.exports = {
-		webpack: (config, { dev }) => {
-		// Perform customizations to config
-		config.plugins.push(
-			new BundleAnalyzerPlugin({
-				analyzerMode: 'disabled',
-				// For all options see https://github.com/th0r/webpack-bundle-analyzer#as-plugin
-				generateStatsFile: true,
-				statsFilename: 'stats.json'
-			})
-		)
-		return config
-	}
-}
+			statsFilename: 'stats.json'
+		})
+	)
+	return config
 }
